eb77c6081dba6f09559da32f74a11c0a3550b922
iamedisonu <iamedisonu250@gmail.com>
2024-07-03 21:55:26 +0200

    added message for the user

--- Assignment1.py
+++ Assignment1.py
@@ -1,5 +1,6 @@
 import numpy
 #I am guessing the user might want to use float numbers, so my variables will hold float not intigers.
+print("This program will help you to power one number to another and also help you to calculate the log2 of the first number")
 x=float(input("Please enter 1st number: "))
 y=float (input("Please enter 2nd number: "))
 print(str(x) + " raised to the power of " + str(y) + " is " + str(x**y))
